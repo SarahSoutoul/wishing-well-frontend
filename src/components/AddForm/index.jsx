45f1deab8f21de458510af62a81e4281582299c2
SarahSoutoul <sarahsoutoul.uk@gmail.com>
2023-02-12 19:06:32 +0000

    remove tags code

--- src/components/AddForm/index.jsx
+++ src/components/AddForm/index.jsx
@@ -1,20 +1,9 @@
-import React, {useState, useEffect} from 'react'
-import Tag from '../Tag';
+import React from 'react'
 
-export default function AddForm({input, setInputText, message, setMessage, tags, setTags}) {
-    const [tagConnection, setTagConnection] = useState([]);
+export default function AddForm({input, setInputText, message, setMessage}) {
 
     function handleInput(e) {
         setInputText(e.target.value)
-    }
-
-    function handleTagConnection(e) {
-        if (e.target.checked) {
-            setTagConnection([...tagConnection, parseInt(e.target.value)])
-        } else {
-            let filteredTags = tagConnection.filter(el => el !== parseInt(e.target.value))
-            setTagConnection(filteredTags)
-        }
     }
 
     function handleSubmit(e) {
@@ -23,7 +12,7 @@ export default function AddForm({input, setInputText, message, setMessage, tags,
         if (input.length > 0) {
             fetch('http://localhost:3000/wishes', {
                 method: 'POST',
-                body: JSON.stringify({text: input, tagIds: tagConnection}),
+                body: JSON.stringify({text: input}),
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8',
                 },
@@ -51,21 +40,9 @@ export default function AddForm({input, setInputText, message, setMessage, tags,
         }
     }
 
-    useEffect(() => {
-        async function loadTags() {
-            const response = await fetch("http://localhost:3000/tags");
-            const data = await response.json();
-            setTags(data)
-        };
-        loadTags();
-    }, [])
-
     return(
         <form>
             <input value={input} type="text" className="wish-input" onChange={handleInput}/>
-            <div className='tag-container'>      
-            {tags.map((tag) => <Tag tag={tag} handleTagConnection={handleTagConnection}/>)}
-            </div>  
             <button type="submit" className="wish-button" onClick={handleSubmit}>Add a wish</button>
             <p className='message'>{message}</p>
         </form>
--- src/pages/AddWishPage/index.jsx
+++ src/pages/AddWishPage/index.jsx
@@ -4,9 +4,8 @@ import { AddForm } from '../../components';
 export default function AddWish() {
     const [input, setInputText] = useState('');
     const [message, setMessage] = useState('');
-    const [tags, setTags] = useState([]);
 
     return (
-        <AddForm input={input} setInputText={setInputText} message={message} setMessage={setMessage} tags={tags} setTags={setTags}/>
+        <AddForm input={input} setInputText={setInputText} message={message} setMessage={setMessage} />
     )
 }
